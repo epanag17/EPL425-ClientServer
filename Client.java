fb742d4ed3dab6ddff358c1f2a3a6692052788e0
epanag17 <epanag17@cs.ucy.ac.cy>
2018-10-16 14:48:54 +0300

    Add files via upload
    The client send request to the server and read the response message that given from the server

--- Client.java
+++ Client.java
@@ -2,123 +2,51 @@ import java.net.*;
 import java.util.Scanner;
 import java.io.*;
 
-class ClientThread extends Thread {
-    private Socket socket;
-    private int user_id;
- 
-    public ClientThread(Socket socket, int user_id) {
-        this.socket = socket;
-        this.user_id = user_id;
-    }
- 
-    public void run() {
-    	OutputStream output;
-		try {
-			InputStream input = socket.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-			
-			output = socket.getOutputStream();
-			//SEND REQUEST TO THE SERVER
-			PrintWriter writer = new PrintWriter(output, true);
-			String hostname= socket.getLocalAddress().toString();
-			int port = socket.getLocalPort();
-			System.out.println(hostname+" "+port);
-			writer.println("HELLO "+hostname+" "+port);
-			
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-      /*  try {
-            InputStream input = socket.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
- 
-            OutputStream output = socket.getOutputStream();
-            PrintWriter writer = new PrintWriter(output, true);
- 
- 
-            String text;
- 
-            do {
-                text = reader.readLine();
-                String reverseText = new StringBuilder(text).reverse().toString();
-                writer.println("Server: " + reverseText);
- 
-            } while (!text.equals("bye"));
- 
-            socket.close();
-        } catch (IOException ex) {
-            System.out.println("Server exception: " + ex.getMessage());
-            ex.printStackTrace();
-        }
-        */
-    }
-}
-
 public class Client {
 
 	public static final int N = 10;
-	
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		if (args.length < 2)
 			return;
 
 		String hostname = args[0];
 		int port = Integer.parseInt(args[1]);
-		System.out.println("hostname: " + hostname);
-		System.out.println("port: "+ port);
-		
-	for(int i=0;i<10;i++){
-			
-			
-		
-		try (Socket socket = new Socket(hostname, port)) {
 
-			System.out.println("Client Thread "+i+", Local Socket Address: "+ socket.getLocalSocketAddress() + ", Remote Socket Address: "+socket.getRemoteSocketAddress());
-			
-			
-			new ClientThread(socket, i);
-			
-			
-			
-			
+		for (int i = 0; i < 10; i++) {
 
-			/*Scanner scan = new Scanner(System.in);
-			String text;
+			try (Socket socket = new Socket(hostname, port)) {
 
-			do {
-				System.out.print("HELLO");
-				text = scan.nextLine();
+				OutputStream output;
+				try {
+					InputStream input = socket.getInputStream();
+					BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 
-				//writer.println(text);
+					output = socket.getOutputStream();
+					// SEND REQUEST TO THE SERVER
+					PrintWriter writer = new PrintWriter(output, true);
+					String IPaddress = socket.getLocalAddress().toString();
 
-				InputStream input = socket.getInputStream();
-				BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+					writer.println("HELLO " + IPaddress + " " + port + " " + i);
+					System.out.println("response to the client: " + reader.readLine());
 
-				String time = reader.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 
-				System.out.println(time);
+				socket.close();
 
-			} while (!text.equals("bye"));
+			} catch (UnknownHostException ex) {
 
-			scan.close();*/
+				System.out.println("Server not found: " + ex.getMessage());
 
-			socket.close();
+			} catch (IOException ex) {
 
-		} catch (UnknownHostException ex) {
+				System.out.println("I/O error: " + ex.getMessage());
+			}
 
-			System.out.println("Server not found: " + ex.getMessage());
-
-		} catch (IOException ex) {
-
-			System.out.println("I/O error: " + ex.getMessage());
 		}
-		
-	}
 	}
 
 }
